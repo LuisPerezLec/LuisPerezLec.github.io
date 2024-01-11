5f0527ba2f1f78e553a47461407085aef70e6b32
LuisPerezLec <luigiperez2002@gmail.com>
2024-01-10 21:45:25 -0600

    Gh Pages

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,6 @@ import styles from "./styles.js";
 export default function App() {
   return (
     <div className='bg-primary w-full overflow-hidden'>
-      <head></head>
       <main>
         <div className={`flex md:flex-row flex-col md:${styles.paddingY+ ' ' +styles.flexCenter} hero-background py-16`}>
           <div className={`bg-white rounded-tl-lg rounded-br-lg w-[80%] xs:w-[440px] p-8`}>
